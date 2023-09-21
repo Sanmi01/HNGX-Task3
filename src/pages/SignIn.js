89e073be4c279de415dd30375119d948f9f022ec
Sanmi01 <supersanmi01@gmail.com>
2023-09-21 23:35:47 +0100

    Removed unused variables

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import {ClipLoader} from "react-spinners";
 import { useAuth } from '../contexts/AuthContext';
-import { auth, provider } from '../firebase/Firebase-Config';
 import EyeIcon from "../components/EyeIcon";
 
 const override = {
